ffbdaf7aabf0c83bf7027356167a9e4308917741
gkgk2412 <gkgk2412@gmail.com>
2026-06-11 13:27:08 +0900

    [손희진] 재료 데이터 추가

---
+++ Assets/Scripts/Data/Ingredient.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class Ingredient
+{
+    public ushort id;
+    public string name;
+    public string description;
+    public string resourceName;
+}
---
+++ Assets/Scripts/Data/IngredientData.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+[Serializable]
+public class IngredientDatabase
+{
+    public List<Ingredient> rows;
+}
+
+// 제이슨 파일을 가져와서 읽고 dictionary 형태의 자료구조로 정리.
+public class IngredientData : IData
+{
+    public Dictionary<ushort, Ingredient> Ingredients { get; private set; } = new();
+
+    private const string ADDRESS = "Ingredient.json";
+    private AsyncOperationHandle<TextAsset> _handle;
+
+    public async Task Initialize()
+    {
+        await LoadR();
+    }
+
+    public void Release()
+    {
+        if (_handle.IsValid())
+        {
+            Addressables.Release(_handle);
+        }
+    }
+
+    private async Task LoadR()
+    {
+        Release();
+
+        _handle = Addressables.LoadAssetAsync<TextAsset>(ADDRESS);
+        TextAsset jsonFile = await _handle.Task;
+
+        if (jsonFile == null)
+        {
+            Debug.LogError("Ingredient.json 없음");
+            return;
+        }
+
+        IngredientDatabase database = JsonUtility.FromJson<IngredientDatabase>(jsonFile.text);
+
+        Ingredients.Clear();
+
+        List<Ingredient> rows = database.rows;
+
+        if (rows == null)
+        {
+            Debug.LogError("Ingredient.json 데이터 없음");
+            return;
+        }
+
+        foreach (var recipe in rows)
+        {
+            if (!Ingredients.ContainsKey(recipe.id))
+            {
+                Ingredients.Add(recipe.id, recipe);
+            }
+        }
+
+        Debug.Log($"{Ingredients.Count}개 로드");
+    }
+}
